00af4a97267532a1f8e85c6ba1a20d24ff2da671
lubomirKavetskiy <lubomirkavetskiy@gmail.com>
2017-08-15 10:29:10 +0300

    finish

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -3,13 +3,42 @@ import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 
 class FilterableProductTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterText: '',
+            inStockOnly: 'false'
+        };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
+
+    handleUserInput(filterTex, inStockOnl) {
+        this.setState({
+            filterText: filterTex,
+            inStockOnly: inStockOnl,
+        });
+
+        console.log('==', filterTex, inStockOnl);
+    }
+
     render() {
         const {products} = this.props;
 
         return(
             <div>
-                <SearchBar />
-                <ProductTable product = {products} />
+                <SearchBar
+                    filterText = {this.state.filterText}
+                    inStockOnly = {this.state.inStockOnly}
+                    onUserInput={this.handleUserInput}
+                />
+                <ProductTable
+                    product = {products}
+                    filterText = {this.state.filterText}
+                    inStockOnly = {this.state.inStockOnly}
+                    onUserInput={this.handleUserInput}
+                />
             </div>
         );
     }
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -9,13 +9,20 @@ class ProductTable extends Component {
         let lastCategory = null;
 
         this.props.product.map(prod => {
+            if((prod.name.indexOf(this.props.filterText) === -1) || (!prod.stocked && this.props.inStockOnly)) {
+                return;
+            }
+
             if(prod.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category = {prod.category}  key={prod.category} />);
+                console.log("category", prod.category);
             }
 
             rows.push(<ProductRow product = {prod} key={prod.name} />);
+            console.log("name", prod.name);
 
             lastCategory = prod.category;
+
         });
 
         return(
